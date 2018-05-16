c0cab82446e093cbb2d18636c3661fc2e2cbbb73
innovator <pondzza@gmail.com>
2018-05-17 00:28:24 +0700

    controller 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    @auth
+                    @authw
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
